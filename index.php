d09b3c89f78765b28638daafadaca9b82dcb1daf
Guillaume Bouyer <guillaume.bouyer@me.com>
2022-05-17 10:35:36 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Poste ton avis !</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    </head>
+    <body>
+        <form action="🤩" method="post" class="w-50 mx-auto pt-5">
+            <div class="mb-3">
+                <label for="avis" class="form-label">Votre avis</label>
+                <textarea class="form-control" id="avis" rows="6" name="avis"></textarea>
+            </div>
+            <button class="btn btn-primary">Envoyer</button>
+        </form>
+    </body>
+</html>
